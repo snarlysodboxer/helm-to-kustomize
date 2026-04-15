1a18909fd500ec730ad5590e89ebc29a511c4d2b
david amick <david@davidamick.com>
2026-04-15 11:30:23 -0700

    Fix flake build

--- flake.nix
+++ flake.nix
@@ -22,7 +22,8 @@
           pname = "helm-to-kustomize";
           version = "0.1.0";
           src = ./.;
-          vendorHash = null;
+          vendorHash = "sha256-komX1AmHt2NoF1x6xsNa2RFkfVzOXfYEMPhT0zwMxjw=";
+          env.CGO_ENABLED = "0";
 
           nativeBuildInputs = [ pkgs.installShellFiles ];
 
@@ -32,6 +33,12 @@
               --zsh <($out/bin/helm-to-kustomize completion zsh) \
               --fish <($out/bin/helm-to-kustomize completion fish)
           '';
+          meta = with pkgs.lib; {
+            description = "An opinionated tool that converts helm template output into kustomize-ready YAML files";
+            homepage = "https://github.com/snarlysodboxer/helm-to-kustomize";
+            license = licenses.mit;
+            mainProgram = "helm-to-kustomize";
+          };
         };
 
         devShells.default = pkgs.mkShell {
